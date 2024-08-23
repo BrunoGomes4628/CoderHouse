55093cbe3b3365c12edd2d0798b2508d6df49355
BrunoGomes4628 <brugomes1712@gmail.com>
2024-08-22 21:48:54 -0300

    adicionando aula 4

---
+++ Aula 4/Aula4.py
@@ -0,0 +1,14 @@
+'''def somaimposto (taxaimposto, custo):
+    resultado = (taxaimposto * custo) + custo
+    return resultado
+
+preco_final = somaimposto(0.2, 500)
+print (preco_final)'''
+
+
+
+
+
+
+
+
