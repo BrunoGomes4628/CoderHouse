0c05cb695433bb2d8299a8cb5204156675b6589d
BrunoGomes4628 <brugomes1712@gmail.com>
2024-08-22 21:51:11 -0300

    adicionando aula 3

--- Aula 3/Aula3.py
+++ Aula 3/Aula3.py
@@ -1,22 +1,22 @@
-'''numero = int(input('Digite um numero inteiro: '))
+numero = int(input('Digite um numero inteiro: '))
 
 if numero % 2:
     print ('impar')
 else:
-    print ('par')'''
+    print ('par')
 
 
-'''frase = input('digite uma frase: ')
+frase = input('digite uma frase: ')
 
 lista = frase.split(' ')
 
 for i in range(len(lista)):
     (lista[i])
     len(lista[i])
-    print(f'a palavra {lista[i]} tem {len(lista[i])} letras')'''
+    print(f'a palavra {lista[i]} tem {len(lista[i])} letras')
 
 
-'''senha = input('digite a senha de 4 digitos: ')
+senha = input('digite a senha de 4 digitos: ')
 
 senha_correta = "1234"
 
@@ -24,32 +24,32 @@ senha_correta = "1234"
 while senha != senha_correta:
     senha = input('digite a senha de 4 digitos: ')
 
-print ('esta certo')'''
+print ('esta certo')
 
 #segunda opção
-'''while True:
+while True:
     if senha == senha_correta:
         break
     senha = input('digite a senha de 4 digitos: ')
 
-print ('esta certo')'''
+print ('esta certo')
 
 
 
 
-'''n = int(input("Digite um número para calcular o fatorial: "))
+n = int(input("Digite um número para calcular o fatorial: "))
 
 fatorial = 1
 for i in range(1,n + 1):
     fatorial *= i
 
-print(f'O fatorial de {n} é {fatorial}')'''
+print(f'O fatorial de {n} é {fatorial}')
 
 
 
 
-'''dicionario = {'janeiro':232, 'fevereiro':321, 'junho':589, 'julho':547}
+dicionario = {'janeiro':232, 'fevereiro':321, 'junho':589, 'julho':547}
 
 for mes, valor in dicionario.items():
-    print (f'{mes}: {valor}')'''
+    print (f'{mes}: {valor}')
 
