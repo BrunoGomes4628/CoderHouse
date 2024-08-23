7d3c3e7b57f6ce957f0fc1516f336c2db2e3a828
BrunoGomes4628 <brugomes1712@gmail.com>
2024-08-22 21:41:22 -0300

    Adicionando aula 2

---
+++ Aula 2/Aula2.py
@@ -0,0 +1,7 @@
+valor_hora = int(input ('digite o salario:'))
+
+numero_trabalhadas_mes = int(input('Quantas horas trabalhou no mes?'))
+
+salario_bruto = valor_hora * numero_trabalhadas_mes
+
+print(salario_bruto)
